4e568ef607967b226a4fe01c40c66067e7f62b99
Martin Collins <martin_p_coll@hotmail.com>
2021-01-15 22:35:07 -0500

    Major revamp of logic. Connections to DB only established when needed. Removed /reconnect method and changed health check (dbconnectioncheck)

--- api/api.py
+++ api/api.py
@@ -17,17 +17,10 @@ except ImportError as e:
 DEBUG = False
 SETTINGS = []
 FLASKAPP = Flask(__name__)
-MySQL_DB_Conn = None
 
 ############################
 
 def exit_app():
-	global SETTINGS
-	global MySQL_DB_Conn
-
-	if MySQL_DB_Conn.is_connected():
-		MySQL_DB_Conn.close()
-		print("INFO: Disconnected from " + SETTINGS['MYSQL_DATABASE'] + " database")
 	print("INFO: Exiting...")
 
 def createJSONResponse(istatus="null", imessage="null", ihttpstatuscode=200):
@@ -84,9 +77,8 @@ def load_settings():
 		print("ERROR: Unable to parse values from settings file: \n" + str(e))
 		sys.exit(1)
 
-def establish_db_connection():
+def new_db_connection():
 	try:
-		global MySQL_DB_Conn
 		global DEBUG
 		global SETTINGS
 
@@ -102,18 +94,21 @@ def establish_db_connection():
 
 		if MySQL_DB_Conn.is_connected():
 			print('INFO: Connected to ' + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  ' MariaDB database.')
+			return MySQL_DB_Conn
+		else:
+			return None
 
 	except Error as e:
 		print("ERROR: Error occurred while trying to connect to the MariaDB database: ", str(e))
-		sys.exit(1)
+		return None
 
-def isParamsValid(icheckcode, istatuscode):
-	global MySQL_DB_Conn
+def isParamsValid(idbconn, icheckcode, istatuscode, icontextcode):
+	cursor = idbconn.cursor(named_tuple=False)
 
-	cursor = MySQL_DB_Conn.cursor(named_tuple=False)
 	query = ("""SELECT EXISTS(SELECT 1
 		FROM check_def cd
 		JOIN status_def sd ON (sd.code = '""" + str(istatuscode) + """' AND sd.active = 'Y')
+		JOIN context_def cd2 ON (cd2.code = '""" + str(icontextcode) + """' AND sd.active = 'Y')
 		WHERE cd.code = '""" + str(icheckcode) +
 		"""' AND cd.active = 'Y');""")
 
@@ -137,29 +132,29 @@ def isParamsValid(icheckcode, istatuscode):
 	except Error as e:
 		print("ERROR: Unexpected error checking whether parameters are valid: ", str(e))
 	finally:
-		if DEBUG:
-			print("DEBUG: Closing isParamsValid MariaDB Cursor")
-		cursor.close()
+		del cursor
 
 def init_app():
 
 	os.environ["FLASK_ENV"] = "development"
 	atexit.register(exit_app)
-	FLASKAPP.run(host='0.0.0.0', debug=DEBUG, use_reloader=False)
+	FLASKAPP.run(host='0.0.0.0', debug=DEBUG, use_reloader=False, threaded=True)
 
 @FLASKAPP.route('/', methods=['GET'])
 def home():
-	global MySQL_DB_Conn
 	global SETTINGS
+	MySQL_DB_Conn = new_db_connection()
 	db_status_str = None
 
-	if MySQL_DB_Conn.is_connected():
-		#Connected to Database
-		db_status_str = '<b><p style="color:green">Connected to ' + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  ' MariaDB database.</p></b>'
+	if MySQL_DB_Conn is not None:
+		if MySQL_DB_Conn.is_connected():
+			#Connected to Database
+			db_status_str = '<b><p style="color:green">Connection test successful to ' + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  ' MariaDB database.</p></b>'
+		else:
+			#Not connected to Database
+			db_status_str = '<b><p style="color:red">Connection test unsuccessful to ' + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  ' MariaDB database.</p></b>'
 	else:
-		#Not connected to Database
-		db_status_str = '<b><p style="color:red">Not connected to the Raspberry Pi Health Check MariaDB database.</p></b>'
-		db_status_str += '<p><button onclick="document.location=\'/reconnect\'">Reconnect</button></p>'
+		db_status_str = '<b><p style="color:red">Unable to connect to the Raspberry Pi Health Check MariaDB database. Check Settings!</p></b>'
 
 	header = "<h1>Raspberry Pi Health Check API</h1>"
 	bodyline1 = "<p>This API provides a method for inserting data into the Raspberry Pi Health Check MariaDB SQL Database.</p>"
@@ -167,92 +162,91 @@ def home():
 
 	html_str = header + bodyline1 + bodyline2
 
+	del MySQL_DB_Conn
 	return html_str
 
-@FLASKAPP.route('/apihealthcheck', methods=['GET'])
-def apiCheck():
-	global MySQL_DB_Conn
+@FLASKAPP.route('/dbconnectioncheck', methods=['GET'])
+def dbConnectionCheck():
 	global SETTINGS
 	global DEBUG
 	db_status_str = None
 
 	if DEBUG:
-		print("DEBUG: API Health Check called.")
-	try:
-		if MySQL_DB_Conn.is_connected():
-			db_status_str = "Connected to " + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  " MariaDB database."
-			return createJSONResponse('0', str(db_status_str),200)
-		else:
-			db_status_str = "Not connected to the Raspberry Pi Health Check MariaDB database."
-			return createJSONResponse('-1', str(db_status_str),500)
-	except Error as e:
-		return createJSONResponse('-1',"API Health Check failed: " + str(e),500)
+		print("DEBUG: DB Connection Check called.")
 
-@FLASKAPP.route('/reconnect', methods=['GET'])
-def reconnectDB():
-	global MySQL_DB_Conn
+	MySQL_DB_Conn = new_db_connection()
 
-	if not MySQL_DB_Conn.is_connected():
+	if MySQL_DB_Conn is not None:
 		try:
-			#Attempt reconnect to Database
-	                MySQL_DB_Conn.reconnect()
+			if MySQL_DB_Conn.is_connected():
+				db_status_str = "Connection test successful to " + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  " MariaDB database."
+				return createJSONResponse('0', str(db_status_str),200)
+			else:
+				db_status_str = "Connection test unsuccessful to " + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  " MariaDB database."
+				return createJSONResponse('-1', str(db_status_str),500)
 		except Error as e:
-			err_str = '<b><p style="color:red">ERROR: Error occurred while trying to connect to the MariaDB database: ' + str(e) + '</b>'
-			return err_str
-
-	return redirect(url_for('home'))
+			return createJSONResponse('-1',"API Health Check failed: " + str(e),500)
+		finally:
+			del MySQL_DB_Conn
+	else:
+		del MySQL_DB_Conn
+		return createJSONResponse('-1',"Unexpected error occurred when trying to connect to MariaDB database.",500)
 
 @FLASKAPP.route('/insert/checklog', methods=['GET', 'POST'])
 def insertCheckLog():
-	global MySQL_DB_Conn
 	global SETTINGS
 	global DEBUG
 
 	if request.method == "GET":
 		parameters = request.args
 
-		for parameter in [ 'code', 'status', 'comment' ]:
+		for parameter in [ 'code', 'status', 'context', 'comment', 'hostname' ]:
 			if parameter not in parameters:
-				return createJSONResponse('-1',"Missing " + str(parameter) + " parameter in call.",406)
+				return createJSONResponse('-1',"Missing \'" + str(parameter) + "\' parameter in call.",406)
 
-		if MySQL_DB_Conn.is_connected():
-			if DEBUG:
-				print("DEBUG: Incoming INSERT CHECKLOG API Request parameters:")
-				for key, value in parameters.items():
-					print(key + ": ", value)
+		MySQL_DB_Conn = new_db_connection()
 
-			if isParamsValid(parameters['code'], parameters['status']):
+		if MySQL_DB_Conn is not None:
 
-				cursor = MySQL_DB_Conn.cursor()
+			if MySQL_DB_Conn.is_connected():
+				if DEBUG:
+					print("DEBUG: Incoming INSERT CHECKLOG API Request parameters:")
+					for key, value in parameters.items():
+						print(key + ": ", value)
 
-				add_check_log = ("INSERT INTO check_log "
-						"(check_code, status_code, comment) "
-						"VALUES (%s, %s, %s)")
+				if isParamsValid(MySQL_DB_Conn, parameters['code'], parameters['status'], parameters['context']):
 
-				data_check_log = (str(parameters['code']), str(parameters['status']), str(parameters['comment']))
+					add_check_log = ("INSERT INTO check_log "
+							"(check_code, status_code, context_code, comment, hostname) "
+							"VALUES (%s, %s, %s, %s, %s)")
 
-				try:
+					data_check_log = (str(parameters['code']), str(parameters['status']), str(parameters['context']), str(parameters['comment']), str(parameters['hostname']))
 
-					cursor.execute(add_check_log, data_check_log)
+					cursor = MySQL_DB_Conn.cursor()
 
-					return createJSONResponse('0',"Successfully inserted record into Check Log table. Row ID: " + str(cursor.lastrowid),200)
-				except Error as e:
-					return createJSONResponse('-1',"Failed to insert record into Check Log table: " + str(e),500)
-				finally:
-					if DEBUG:
-						print("DEBUG: Closing insertCheckLog MariaDB Cursor")
-					cursor.close()
+					try:
+						cursor.execute(add_check_log, data_check_log)
+
+						return createJSONResponse('0',"Successfully inserted record into Check Log table. Row ID: " + str(cursor.lastrowid),200)
+					except Error as e:
+						return createJSONResponse('-1',"Failed to insert record into Check Log table: " + str(e),500)
+					finally:
+						del cursor
+						del MySQL_DB_Conn
+				else:
+					del MySQL_DB_Conn
+					return createJSONResponse('-1',"Failed to insert record into Check Log table, input parameters are not valid.",500)
 			else:
-				return createJSONResponse('-1',"Failed to insert record into Check Log table, input parameters are not valid.",500)
+				del MySQL_DB_Conn
+				return createJSONResponse('-1',"Failed to insert record into Check Log table, not connected to MariaDB database.",500)
 		else:
-			return createJSONResponse('-1',"Failed to insert record into Check Log table, not connected to MariaDB database.",500)
-
+			del MySQL_DB_Conn
+			createJSONResponse('-1',"Unexpected error occurred when trying to connect to MariaDB database.",500)
 
 if __name__ == "__main__":
 
 	if (DEBUG):
 		print("INFO: DEBUG mode enabled!")
 	load_settings()
-	establish_db_connection()
 	init_app()
 	sys.exit(0)
