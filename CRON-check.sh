f6d09ecb638903dc56327be320361e1d657a670d
Martin Collins <martin_p_coll@hotmail.com>
2021-03-31 00:02:07 -0400

    Initial commit of CRON-Check.sh

---
+++ CRON-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd "$(dirname "$0")";
+
+###### Script to be run by CRON to call the RPi HealthCheck API DB Connection Test method and report issues via email
+SEND_EMAIL_IF_OKAY=$1
+TO_EMAIL=mcollins1290@gmail.com
+
+response=$(curl -G -s http://raspberrypi2.nyave:5000/dbconnectioncheck)
+retVal=$?
+
+if [ $retVal -ne 0 ]; then
+	echo "No response from RPi HealthCheck API." | mail -s "ALERT: RPi HealthCheck DB Connection Test" $TO_EMAIL
+else
+	status=$(echo $response | jq -r '.status')
+	message=$(echo $response | jq -r '.message')
+
+	if [ $status -ne 0 ]; then
+		printf "Status: ${status}\nMessage: ${message}" | mail -s "ALERT: RPi HealthCheck DB Connection Test" $TO_EMAIL
+	else
+		if [ "$SEND_EMAIL_IF_OKAY" = true ] ; then
+			printf "Status: ${status}\nMessage: ${message}" | mail -s "OK: RPi HealthCheck DB Connection Test" $TO_EMAIL
+		fi
+	fi
+fi
+
+exit 0
