46f3fbf35232da469943a2046e0e89e8c8260161
Martin Collins <martin_p_coll@hotmail.com>
2021-04-25 11:49:36 -0400

    Minor modification to email subject

--- CRON-check.sh
+++ CRON-check.sh
@@ -9,16 +9,16 @@ response=$(curl -G -s http://raspberrypi2.nyave:5000/dbconnectioncheck)
 retVal=$?
 
 if [ $retVal -ne 0 ]; then
-	echo "No response from RPi HealthCheck API." | mail -s "ALERT: RPi HealthCheck DB Connection Test" $TO_EMAIL
+	echo "No response from RPi HealthCheck API." | mail -s "ALERT - RPi HealthCheck DB Connection Test" $TO_EMAIL
 else
 	status=$(echo $response | jq -r '.status')
 	message=$(echo $response | jq -r '.message')
 
 	if [ $status -ne 0 ]; then
-		printf "Status: ${status}\nMessage: ${message}" | mail -s "ALERT: RPi HealthCheck DB Connection Test" $TO_EMAIL
+		printf "Status: ${status}\nMessage: ${message}" | mail -s "ALERT - RPi HealthCheck DB Connection Test" $TO_EMAIL
 	else
 		if [ "$SEND_EMAIL_IF_OKAY" = true ] ; then
-			printf "Status: ${status}\nMessage: ${message}" | mail -s "OK: RPi HealthCheck DB Connection Test" $TO_EMAIL
+			printf "Status: ${status}\nMessage: ${message}" | mail -s "OK - RPi HealthCheck DB Connection Test" $TO_EMAIL
 		fi
 	fi
 fi
